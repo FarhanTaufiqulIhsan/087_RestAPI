536413227a09c14cca1916e8b7ee428dbcb5310a
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-28 12:53:47 +0700

    Membuat column beserta stylnya pada fun EntryKontakBody

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -85,4 +85,11 @@ fun EntryKontakBody(
     onSiswaValueChange: (InsertUiEvent) -> Unit,
     noSaveClick: () -> Unit,
     modifier: Modifier = Modifier
-){}
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+
+    }
+}
