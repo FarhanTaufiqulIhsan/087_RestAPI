b9ea51b7e7099e989934d2a2406c9b19e3432387
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-28 12:03:33 +0700

    Membuat fun FormInputSiswa

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -12,3 +12,7 @@ object DestinasiEntry : DestinasiNavigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntryKontakScreen(){}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(){}
