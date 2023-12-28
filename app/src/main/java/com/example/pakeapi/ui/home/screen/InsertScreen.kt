b611423100747cdbfe8b2f30ff26f6e2eb0bd97e
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-28 12:06:35 +0700

    Menambahkan Column pada fun FormInputSiswa

--- app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/InsertScreen.kt
@@ -1,8 +1,11 @@
 package com.example.pakeapi.ui.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pakeapi.navigation.DestinasiNavigasi
 import com.example.pakeapi.ui.home.viewmodel.InsertUiEvent
 
@@ -22,4 +25,11 @@ fun FormInputSiswa(
     modifier: Modifier = Modifier,
     onValueChange : (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
-){}
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+    }
+}
